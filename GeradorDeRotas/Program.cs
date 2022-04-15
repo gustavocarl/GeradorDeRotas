1f1745be85fd9ba23a686495df6614d39991ca03
Gustavo Henrique Carl Dos Santos <gustavo_carl@live.com>
2022-04-15 01:02:37 -0300

    Adicionar arquivos de projeto.

---
+++ GeradorDeRotas/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeradorDeRotas
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CultureInfo ptBr = new CultureInfo("pt-BR");
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Model/Pessoa.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Model
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
